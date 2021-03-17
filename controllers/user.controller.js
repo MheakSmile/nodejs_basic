7fafd47092a0936fe798a2ef9771f84cedfb886c
Jirawat Puedjantuek <mheakzaza@hotmail.com>
2021-03-17 21:53:16 +0700

    homework

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,6 +4,8 @@ import fetch from "node-fetch"
  * @param {import("express").Request} req 
  * @param {import("express").Response} res 
  */
+
+let array =[]
 export const Login = (req,res) => {
     //const {username,password} = req.query
     //localhost->8080/api/v1/users/login/mheak/5555
@@ -14,6 +16,7 @@ export const Login = (req,res) => {
         password
     })
 }
+
 export const Users = async(req,res) => {
     const data = await fetch("https://jsonplaceholder.typicode.com/users")
     const json = await data.json()
@@ -21,9 +24,25 @@ export const Users = async(req,res) => {
     const {msg} = req.body
     const value = json.filter((user) => user.id == id)
     res.json({
-     msg,
+    msg,
      data : value,
     })
 }
 
-export default { Login, Users }
+export const Register = (req,res) => {
+    const {username} = req.body
+    array.push(username)
+    res.json({        
+        username,        
+    })
+}
+
+export const Search = (req,res) => {
+    const {username} = req.params
+    const data = array.filter((name) => name == username)
+    res.json({
+        username : data,
+    })
+}
+
+export default { Login, Users,Register,Search }
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,13 @@
 import express from "express"
-import {Login,Users} from "../controllers/user.controller.js"
+import {Login,Users,Register,Search} from "../controllers/user.controller.js"
 const user_route = express.Router()
 
 user_route.post("/login",(req,res)=>Login(req,res))
-user_route.get("/:id",(req,res)=>Users(req,res))
+//user_route.get("/:id",(req,res)=>Users(req,res))
+user_route.post("/Register",(req,res)=>Register(req,res))
+user_route.get("/",(req,res)=>Users(req,res))
+user_route.post("/Search/:username",(req,res)=>Search(req,res))
+
+
 
 export default user_route
